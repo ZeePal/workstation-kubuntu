ed0bf825b03a6b39ffb9472f016297985baa0969
ZeePal <pallsjh@gmail.com>
2026-03-03 22:05:39 +1100

    updated testing, outputting & some formatting

--- callback_plugins/yaml_quiet.py
+++ callback_plugins/yaml_quiet.py
@@ -1,4 +1,7 @@
-from ansible_collections.community.general.plugins.callback.yaml import CallbackModule as CommunityYaml
+from ansible import constants as C
+from ansible.executor.task_result import CallbackTaskResult
+from ansible.playbook.task_include import TaskInclude
+from ansible.plugins.callback.default import CallbackModule as CallbackDefaultModule
 
 DOCUMENTATION = """
     name: yaml_quiet
@@ -13,7 +16,7 @@ DOCUMENTATION = """
 """
 
 
-class CallbackModule(CommunityYaml):
+class CallbackModule(CallbackDefaultModule):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = "stdout"
     CALLBACK_NAME = "yaml_quiet"
@@ -40,3 +43,34 @@ class CallbackModule(CommunityYaml):
     def v2_playbook_on_stats(self, stats):
         if self._display.verbosity != 0:
             return super().v2_playbook_on_stats(stats)
+
+    def v2_runner_on_ok(self, result: CallbackTaskResult) -> None:
+        if isinstance(result.task, TaskInclude):
+            return super().v2_runner_on_ok(result)
+        elif result.result.get("changed", False):
+            if self._last_task_banner != result.task._uuid:
+                self._print_task_banner(result.task)
+
+            msg = ""
+            if "cmd" in result.result:
+                if "stdout" in result.result:
+                    msg += result.result["stdout"]
+                if "stderr" in result.result:
+                    msg += result.result["stderr"]
+            if msg:
+                self._display.display(msg)
+        return super().v2_runner_on_ok(result)
+
+    def v2_runner_on_failed(self, result: CallbackTaskResult, ignore_errors: bool = False) -> None:
+        if self._last_task_banner != result.task._uuid:
+            self._print_task_banner(result.task)
+
+        msg = ""
+        if "cmd" in result.result:
+            if "stdout" in result.result:
+                msg += result.result["stdout"]
+            if "stderr" in result.result:
+                msg += result.result["stderr"]
+        if msg:
+            self._display.display(msg)
+        return super().v2_runner_on_failed(result, ignore_errors=ignore_errors)
